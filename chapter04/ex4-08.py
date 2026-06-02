225db571ac87dcfa286edea30cedc5189df2b9e9
itc-s26007 <s26007@std.it-college.ac.jp>
2026-06-02 07:52:38 +0000

    第４章　例題13まで

---
+++ chapter04/ex4-08.py
@@ -0,0 +1,6 @@
+age = input("Enter your age:")
+int_age = int(age)
+if int_age < 21:
+    ptint("You are young!")
+else:
+    print("Wow, you are old!")
---
+++ chapter04/ex4-10.py
@@ -0,0 +1,6 @@
+n = input("type a number:")
+n = int(n)
+if n % 2 == 0:
+    print("n is even.")
+else:
+    print("n is odd.")
