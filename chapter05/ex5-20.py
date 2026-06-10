b2dc0c153f2a5c64b5f0545ed6536591eb7bd3ec
itc-s26007 <s26007@std.it-college.ac.jp>
2026-06-10 07:58:43 +0000

    P70-P86 第５章演習 終了

---
+++ chapter05/ex5-20.py
@@ -0,0 +1,3 @@
+bill = {"Bill Gates": "chariable"}
+print("Bill Gates" in bill)
+print("Bill Doors" not in bill)
---
+++ chapter05/ex5-26.py
@@ -0,0 +1,8 @@
+bday = {"Hemingway": "7.21.1899",
+        "fitzgeraid": "9.24.1896"}
+
+my_list = [bday]
+print(my_list)
+
+my_tuple = (bday, )
+print(my_tuple)
